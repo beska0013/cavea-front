f03bfaaeccf0d50ea154b6e3c1fd13a1203fbc3b
beska0013 <gugushvili.beso0013@gmail.com>
2023-03-03 16:21:27 +0400

    start: add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,35 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<p class="h1">app works</p>
+  <table class="table table-striped">
+    <thead>
+    <tr>
+      <th>Item Name</th>
+      <th>Quantity</th>
+      <th>Price</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td>Item 1</td>
+      <td>2</td>
+      <td>$10.00</td>
+    </tr>
+    <tr>
+      <td>Item 2</td>
+      <td>1</td>
+      <td>$5.00</td>
+    </tr>
+    <tr>
+      <td>Item 3</td>
+      <td>3</td>
+      <td>$15.00</td>
+    </tr>
+    </tbody>
+  </table>
+  <router-outlet></router-outlet>`,
+
 })
 export class AppComponent {
   title = 'cavea-front';
